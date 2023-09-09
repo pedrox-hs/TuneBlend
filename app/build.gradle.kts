12e47596583762f923ba1a8dd07658d79faba920
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-09 03:31:11 -0300

    Bump compile SDK
    Signed-off-by: Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.trilobitech.tuneblend"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.trilobitech.tuneblend"
         minSdk = 24
-        targetSdk = 33
+
         versionCode = 1
         versionName = "1.0"
 
