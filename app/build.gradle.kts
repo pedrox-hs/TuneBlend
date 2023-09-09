7361b8dab7b6b188a8d78c39a4c9104922886623
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-09 02:40:03 -0300

    Replace dependecies declaration by catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -36,11 +36,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.bundles.android.layout)
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.bundles.testing.unit)
+
+    androidTestImplementation(libs.bundles.testing.android)
 }
