b68bbba0287413dff4ebc3e6fa4c2308e590bf27
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-09 06:58:00 -0300

    Centralize common module config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,9 @@ plugins {
 
 android {
     namespace = "com.trilobitech.tuneblend"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.trilobitech.tuneblend"
-        minSdk = 24
 
         versionCode = 1
         versionName = "1.0"
@@ -25,13 +23,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
 }
 
