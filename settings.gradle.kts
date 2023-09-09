b2e38b26623f12452ef678bb122ab113846d1ac2
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-09 02:39:17 -0300

    Enable gradle version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,16 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
+    }
+
+    versionCatalogs {
+        create("deps") {
+            from(files("gradle/libs.versions.toml"))
+        }
     }
 }
 
