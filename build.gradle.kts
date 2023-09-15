10d42385051ae8643208384d5d508eb4fd54d043
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-15 20:35:06 -0300

    Apply detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.sonarCloud) apply false
+    alias(libs.plugins.detekt) apply false
 }
 
 apply {
+    from("gradle/scripts/detekt.gradle")
     from("gradle/scripts/sonar.gradle")
 }
