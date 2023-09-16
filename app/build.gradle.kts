98e5bb84c859816327beced0c3287100f9ad512f
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-16 03:05:13 -0300

    Refact gradle scripts
    - centralize android config
    - centralize kotlin config
    - bump to JDK 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,10 @@ plugins {
 }
 
 android {
-    namespace = "com.trilobitech.tuneblend"
-    compileSdk = 34
+    resourcePrefix = ""
 
     defaultConfig {
         applicationId = "com.trilobitech.tuneblend"
-        minSdk = 24
 
         versionCode = 1
         versionName = "1.0"
@@ -26,12 +24,11 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
