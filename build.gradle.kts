4ad4054a2b29c91cb59b6ac4b602d54039fcb1d4
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-16 19:08:17 -0300

    Add paparazzi plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.sonarCloud) apply false
+    alias(libs.plugins.papanazzi) apply false
     alias(libs.plugins.detekt) apply false
 }
 
