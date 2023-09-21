8c78f790554da061575b45d9e5ceb1b34ce7b8a7
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-09-21 12:07:16 -0300

    Create root module plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.module.root)
 
     alias(libs.plugins.sonarCloud) apply false
     alias(libs.plugins.paparazzi) apply false
